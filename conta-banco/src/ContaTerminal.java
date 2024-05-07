345dc1129a67690ddac44344cee520ec972cfdc4
LCS-TECH <macacom149@gmail.com>
2024-05-07 16:47:28 -0300

    projeto conta bancaria concluido

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,29 @@
+import java.util.*;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        String agencia;
+        String nomeCliente;
+        int numero;
+        double saldo;
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Por favor, digite o numero da sua agência.");
+        agencia = sc.nextLine();
+
+        System.out.println("Por favor, digite o numero da sua conta");
+        numero = sc.nextInt();
+        sc.nextLine();
+        System.out.println("Por favor, informe seu nome.");
+        nomeCliente = sc.nextLine();
+
+        saldo = 0;
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é "
+                + agencia + " conta " + numero + " seu saldo de " + saldo + " já está disponível para saque.");
+
+        sc.close();
     }
 }
